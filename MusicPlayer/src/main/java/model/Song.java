953173e31a48fcdf4fa5072aa3bfa9d0386e2c75
ic-luc <flucic097@gmail.com>
2026-03-12 10:11:22 +0100

    added 'songId' to the constructor and generated getters & setters

--- MusicPlayer/src/main/java/model/Song.java
+++ MusicPlayer/src/main/java/model/Song.java
@@ -6,9 +6,42 @@ public class Song {
     private String artist;
     private int duration;   // duration of song in seconds
 
-    public Song(String title, String artist, int duration) {
+    public Song(int songId, String title, String artist, int duration) {
+        this.songId = songId;
         this.title = title;
         this.artist = artist;
         this.duration = duration;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getDuration() {
+        return duration;
     }
 }
