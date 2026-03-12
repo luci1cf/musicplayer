1fc11d09607f8a10923d7c09a1c75084adabc23c
ic-luc <flucic097@gmail.com>
2026-03-12 10:19:38 +0100

    declared variables for the class 'Artist' and wrote a constructor for it (also instantiated array lists for songs & albums)

--- MusicPlayer/src/main/java/model/Artist.java
+++ MusicPlayer/src/main/java/model/Artist.java
@@ -1,4 +1,18 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Artist {
+    private int artistId;
+    private String name;
+    private List<Song> songs;
+    private List<Album> albums;
+
+    public Artist(int artistId, String name, List<Song> songs, List<Album> albums) {
+        this.artistId = artistId;
+        this.name = name;
+        this.songs = new ArrayList<>();
+        this.albums = new ArrayList<>();
+    }
 }
