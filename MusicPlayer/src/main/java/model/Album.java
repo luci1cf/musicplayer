f7d035df41dea01e08429471257c8c96f2864426
ic-luc <flucic097@gmail.com>
2026-03-12 10:52:32 +0100

    declared variables for class 'Album' and generated a constructor for it

--- MusicPlayer/src/main/java/model/Album.java
+++ MusicPlayer/src/main/java/model/Album.java
@@ -1,4 +1,21 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Album {
+    private int albumId;
+    private String albumName;
+    private String artist;
+    private LocalDate releaseDate;
+    private List<Song> albumSongs = new ArrayList<>();
+    private int albumDuration;  // duration of the playlist in second; later on converted to mins and # of playlists
+
+    public Album(int albumId, String albumName, String artist, LocalDate releaseDate) {
+        this.albumId = albumId;
+        this.albumName = albumName;
+        this.artist = artist;
+        this.releaseDate = releaseDate;
+    }
 }
