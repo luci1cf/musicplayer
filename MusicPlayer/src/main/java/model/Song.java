b3d261e65a73d3c5418dcfd3963ef43a4f186d45
ic-luc <flucic097@gmail.com>
2026-03-12 10:08:35 +0100

    declared the needed values for class 'Song' and created a constructor for it

--- MusicPlayer/src/main/java/model/Song.java
+++ MusicPlayer/src/main/java/model/Song.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Song {
+    private int songId;
+    private String title;
+    private String artist;
+    private int duration;   // duration of song in seconds
+
+    public Song(String title, String artist, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
 }
