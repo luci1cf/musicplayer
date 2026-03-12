7551a29a8e73850cdd7a1d1ba413a66e0e518d38
ic-luc <flucic097@gmail.com>
2026-03-12 10:48:52 +0100

    generated getters & setters (for 'playlistSongs' & 'playlistDuration' only getters)

--- MusicPlayer/src/main/java/model/Playlist.java
+++ MusicPlayer/src/main/java/model/Playlist.java
@@ -13,4 +13,28 @@ public class Playlist {
         this.playlistId = playlistId;
         this.playlistName = playlistName;
     }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public List<Song> getPlaylistSongs() {
+        return playlistSongs;
+    }
+
+    public String getPlaylistDuration() {
+        return playlistDuration;
+    }
 }
