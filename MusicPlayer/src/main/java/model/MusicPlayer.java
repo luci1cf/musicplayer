37577c010e90cddf8d7679ee5588f9e704f34368
ic-luc <flucic097@gmail.com>
2026-03-17 08:38:23 +0100

    wrote methods for adding and removing songs to/out of the queue

--- MusicPlayer/src/main/java/model/MusicPlayer.java
+++ MusicPlayer/src/main/java/model/MusicPlayer.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.List;
+
 public class MusicPlayer {
+    private List<Song> queue;
+
+    public void addSongToQueue(Song song) {
+        queue.add(song);
+    }
+
+    public void removeSongFromQueue(Song song) {
+        if (queue.contains(song)) {
+            queue.remove(song);
+        } else {
+            System.out.printf("The song %s by %s is not in the queue.", song.getTitle(), song.getArtist());
+        }
+    }
 }
