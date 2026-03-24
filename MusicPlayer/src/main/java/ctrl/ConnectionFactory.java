08fbd24a54e4fe62bbc59534ccb4133950354226
ic-luc <flucic097@gmail.com>
2026-03-24 10:53:43 +0100

    added 'getProperties()' which load Db properties from the 'db.properties' file or writes default properties into a new file in case 'db.properties' doesn't exist

--- MusicPlayer/src/main/java/ctrl/ConnectionFactory.java
+++ MusicPlayer/src/main/java/ctrl/ConnectionFactory.java
@@ -1,5 +1,6 @@
 package ctrl;
 
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,7 +8,7 @@ import java.util.Properties;
 
 public class ConnectionFactory {
     // Loads database properties from file or creates default ones
-    private final static Properties prop = getProperty();
+    private final static Properties prop = getProperties();
 
     // Read database configuration with fallback default values (Didn't write the lines fully out of privacy reasons)
     private final static String driver = prop.getProperty("DB_Driver", "oracle.jdbc.OracleDriver");
@@ -41,5 +42,36 @@ public class ConnectionFactory {
      */
     public static Connection getInstance() {
         return con;
+    }
+
+    /*
+        Loads database properties from "db.properties".
+        If the file doesn't exist, default properties will be created
+        and written to a new file
+
+        @return Properties object containing DB configuuration
+     */
+
+    private static Properties getProperties() {
+        Properties p = new Properties();
+        String fileName = "db.properties";
+
+        try {
+            p.load(new FileReader(fileName));
+
+        } catch (IOException e) {
+            p.setProperty("DB_Driver", "oracle.jdbc.OracleDriver");
+            p.setProperty("DB_URL", "...");
+            p.setProperty("DB_User", "...");
+            p.setProperty("DB_Passwd", "...");
+
+            try {
+                p.store(new FileWriter(fileName), "lmaa");
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        return p;
     }
 }
