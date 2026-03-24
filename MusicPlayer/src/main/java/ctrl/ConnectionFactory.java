3bddc6eb4e5ce60324e00f35d500d487062eaca2
ic-luc <flucic097@gmail.com>
2026-03-24 10:20:18 +0100

    loading of database properties from file and reading of DB configuration

--- MusicPlayer/src/main/java/ctrl/ConnectionFactory.java
+++ MusicPlayer/src/main/java/ctrl/ConnectionFactory.java
@@ -1,4 +1,16 @@
 package ctrl;
 
+import java.util.Properties;
+
 public class ConnectionFactory {
+    // Loads database properties from file or creates default ones
+    private final static Properties prop = getProperty();
+
+    // Read database configuration with fallback default values (Commented these lines and didn't write it fully out of privacy reasons)
+    private final static String driver = prop.getProperty("DB_Driver", "oracle.jdbc.OracleDriver");
+    /*
+    private final static String url = prop.getProperty("DB_URL", "...");
+    private final static String user = prop.getProperty("DB_User", "...");
+    private final static String passwd = prop.getProperty("DB_Passwd", "...");
+    */
 }
