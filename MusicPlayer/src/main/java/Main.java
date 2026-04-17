26289977ceb84c85c08dec8a8068cd507860d588
ic-luc <flucic097@gmail.com>
2026-04-17 10:22:16 +0200

    created a layout for dummy tests for my project

--- MusicPlayer/src/main/java/Main.java
+++ MusicPlayer/src/main/java/Main.java
@@ -1,8 +1,65 @@
 import app.UI;
+import model.*;
 
 public class Main {
     public static void main(String[] args) {
-        UI ui = new UI();
-        ui.start();
+//      UI ui = new UI();
+//      ui.start();
+    }
+
+    public static void runTests() {
+        int success = 0;
+        int failed = 0;
+
+        if (testCreatePlaylist()) success++; else failed++;
+        if (testChangePlaylistName()) success++; else failed++;
+        if (testAddSongToPlaylist()) success++; else failed++;
+        if (testRemoveSongFromPlaylist()) success++; else failed++;
+        if (testDeletePlaylist()) success++; else failed++;
+
+
+        System.out.println("---- RESULTS ----");
+        System.out.println("SUCCESS: ", success);
+        System.out.println("FAIL: ", failed);
+    }
+
+    public static boolean testCreatePlaylist() {
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean testChangePlaylistName() {
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean testAddSongToPlaylist() {
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean testRemoveSongFromPlaylist() {
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean testDeletePlaylist() {
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
